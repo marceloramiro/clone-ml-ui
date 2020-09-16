4fd724c58e0f02ee24748702a1797b3d0d4a30a7
Marcelo Ramiro <marcelo.jdeveloper@outlook.com>
2020-09-15 23:43:31 -0300

    web files

---
+++ docs/precache-manifest.502dba40115039c0374b802cf30373e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfec6842eb7223016f98dce80a7b41eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba85e68c32517136bbcb",
+    "url": "/static/js/2.113c2d68.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.113c2d68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b853d10a8453cac4d05",
+    "url": "/static/js/main.76d70a2b.chunk.js"
+  },
+  {
+    "revision": "140ac787906f27ef248e",
+    "url": "/static/js/runtime-main.ff723d3d.js"
+  },
+  {
+    "revision": "7df03694fbb128e2b06e30c2f883f42e",
+    "url": "/static/media/tshirt.7df03694.png"
+  }
+]);
